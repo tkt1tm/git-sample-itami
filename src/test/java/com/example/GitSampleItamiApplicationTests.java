4c8f8a888f30110a4add91be831ee36562c9e98e
tkt1tm <takuto.itami.l@gmail.com>
2025-05-19 11:46:30 +0900

    first commit

---
+++ src/test/java/com/example/GitSampleItamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleItamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
